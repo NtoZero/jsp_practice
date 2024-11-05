dc02e6559963320ff34f1c2dfbca7106f47bc6df
st.lee <dev.st1027@gmail.com>
2024-11-05 22:06:51 +0900

    feat(처음 시작하는 JSP 서블릿) : ~ 124p 3.요청정보와 응답정보

--- src/main/java/learn/jsp_practice/init/jsp_servlet/ThirdServlet.java
+++ src/main/java/learn/jsp_practice/init/jsp_servlet/ThirdServlet.java
@@ -8,18 +8,18 @@ import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.io.PrintWriter;
 
-@WebServlet("/second")
-public class SecondServlet extends HttpServlet {
+@WebServlet("/third")
+public class ThirdServlet extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        resp.setContentType("text/html; charset=utf-8");
 
-        resp.setContentType("text/html");
+        System.out.println("req.getScheme() : " + req.getScheme());
+        System.out.println("req.getServerName() : " + req.getServerName());
+        System.out.println("req.getRemoteAddr() : " + req.getRemoteAddr());
 
-        System.out.println("SecondServlet doGet!");
         PrintWriter out = resp.getWriter();
-        out.println("<html><head><title>Test</title></head>");
-        out.println("<body><h1>have a nice day!</h1></body");
-        out.println("</html>");
+        out.println("<h1>좋은 하루!</h1>");
         out.close();
     }
 }
